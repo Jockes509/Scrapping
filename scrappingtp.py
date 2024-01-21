8d376a6d2c3c316fc66e55bf9ee1e34d9236659d
Jockes509 <148838591+Jockes509@users.noreply.github.com>
2024-01-20 21:50:33 -0500

    commit

--- scrappingtp.py
+++ scrappingtp.py
@@ -1,17 +1,38 @@
 
 import requests
 from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
 
 
 url='https://lenouvelliste.com/'
-#def scrape_journal(url):
-    # Envoyer une requête pour récupérer le contenu de la page
- #   response = requests.get(url)
 
 paj= requests.get(url)
 
 
 soup=bs(paj.text,'html.parser')
 
-print(soup)
 
+
+#d'abord jwenn articles yo
+article_tags = soup.find_all('div', class_='lnv-featured-article-sm')
+
+# boucle pou pran contenu ke nou bezwen yo
+for tag in article_tags:
+        # titre yo
+        title = tag.find('h1').text.strip()
+
+        # imaj yo
+        image = urljoin(url, tag.find('img')['src']) if tag.find('img') else None
+
+        # description yo si genyen
+        description = tag.find('p', class_='article-intro').text.strip() if tag.find('p', class_='article-intro') else None
+
+        # lyen yo
+        link = urljoin(url, tag.find('a')['href']) if tag.find('a') else None
+
+        
+        print("Titre article:", title)
+        print("Image article:", image)
+        print("Description article:", description)
+        print("Lien article:", link)
+        print("\n\n")
